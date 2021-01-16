833b2b55dd37695fed076e559c161fb9d8cd32a5
mspencersmith <mspencersmith@outlook.com>
2021-01-16 20:05:15 +0000

    Refactored web_scraper.py

--- get_data.py
+++ get_data.py
@@ -6,24 +6,24 @@ start = time.time()
 
 f = 'data/test.csv'
 print(f'Writing to {f}..')
-ws = WebScraper()
+
 
 asp_url = 'https://www.aspects-holidays.co.uk/cottages/in/newquay/start/2021-12-25/sleeps/4/los/7/sorting/price-ascending'
-ws.aspects(f, asp_url, write=True)
+WebScraper(f, asp_url, 'aspects', write=True)
 print('\n\nAppending aspects.com data')
 for i in tqdm(range(2, 4)):
-    ws.aspects(f, asp_url, i)
+    WebScraper(f, asp_url, 'aspects', i)
 
 air_url = 'https://www.airbnb.co.uk/s/Fistral-Beach--Newquay/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&checkin=2021-12-25&checkout=2022-01-01&adults=4&source=structured_search_input_header&search_type=filter_change&ne_lat=50.45471946276464&ne_lng=-5.009513859244635&sw_lat=50.36223875919588&sw_lng=-5.140892040057679&zoom=12&search_by_map=true&place_id=ChIJD2v36sUPa0gRPb4zR4nEyrk'
-# ws.airbnb(f, air_url, write=True)
+WebScraper(f, air_url, 'airbnb')
 print('\n\nAppending airbnb.com data')
 for i in tqdm(range(20, 301, 20)): # Offset starts at 20 amd increases by 20
-    ws.airbnb(f, air_url, offset=i)
+    WebScraper(f, air_url, 'airbnb', i)
 
 boo_url = 'https://www.booking.com/searchresults.en-gb.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQ_YAQHoAQH4AQuIAgGoAgO4Aq7B__4FwAIB0gIkNTIyZjhlMDItNWM3ZC00YzQ5LThlYzAtYmEzN2QyMzk0Zjlj2AIG4AIB&sid=333f4c345becd6ba8ddebb42f1635dc2&tmpl=searchresults&ac_click_type=b&ac_position=0&checkin_month=12&checkin_monthday=25&checkin_year=2021&checkout_month=1&checkout_monthday=1&checkout_year=2022&class_interval=1&dest_id=-2604050&dest_type=city&dtdisc=0&from_sf=1&group_adults=4&group_children=0&iata=NQY&inac=0&index_postcard=0&label_click=undef&no_rooms=2&order=price&percent_htype_apt=1&postcard=0&raw_dest_type=city&room1=A%2CA&room2=A%2CA&sb_price_type=total&search_selected=1&shw_aparth=1&slp_r_match=0&srpvid=af3004af35b70105&ss=Newquay%2C%20Cornwall%2C%20United%20Kingdom&ss_all=0&ss_raw=newq&ssb=empty&sshis=0&top_ufis=1&nflt=ht_id%3D201%3Bht_id%3D220%3Bht_id%3D213%3B&rsf='
-# ws.booking(f, boo_url, write=True)
+WebScraper(f, boo_url, 'booking')
 for i in tqdm(range(25, 51, 25)): # Offset starts at 25 and increases by 25 
-    ws.booking(f, boo_url, offset=i)
+    WebScraper(f, boo_url, 'booking', i)
 
 finish = time.time()
 secs = round((finish - start), 2)
--- web_scraper.py
+++ web_scraper.py
@@ -7,91 +7,76 @@ from tqdm import tqdm
 class WebScraper:
     """Scrapes holiday websites and returns name, price, type of accommodation, number of rooms"""
 
-    def aspects(self, filename, url, page=None, write=False):
-        """Scraper for https://www.aspects-holidays.co.uk/"""
+    def __init__(self, filename, url, website, page=None, write=False):
         self.url = url
-        if write:
-            self._edit_file(filename, 'w')        
-        else:
-            self._set('asp', page)
-        response, soup = self._get_response(self.url)
+        self.website = website
+        self.filename = filename
+        self.page = page
+        self.write = write
+        if page:        
+            self.create_url() #Creates url for extra pages
+        self.scrape()
+
+    def scrape(self):
+        """Scrapes website outputs to csv"""
+        if self.write:
+            self.edit_file('w') # Writes header
+
+        response, soup = self.get_response()
         
-        for item in soup.select('.property-box-inner'):
+        for item in soup.select(self.block): # Selects block of html attributes are stored
             try:
-                self.get_attr_asp(item) # Gets aspects attributes
-                self._edit_file(filename, 'a')
+                self.get_attr(item)
+                self.edit_file('a') # Appends to file
             except UnicodeEncodeError as e:
                 print(e)
-                self.get_attr_asp(item)
-                self._edit_file(filename, 'a', encode=True)
+                self.get_attr(item)
+                self.edit_file('a', encode=True) # Appends to file with encoding
 
-    def airbnb(self, filename, url, offset=None, write=False):
-        """Scraper for https://www.airbnb.co.uk/"""
-        self.url = url
-        if write:
-            self._edit_file(filename, 'w')
-        else:
-            self._set('air', offset)
-        response, soup = self._get_response(self.url)
-        
-        for item in soup.select('._8s3ctt'):
-            try:
-                self.get_attr_air(item) # Gets airbnb attributes
-                self._edit_file(filename, 'a')
-            except UnicodeEncodeError as e:
-                print(e)
-                self.get_attr_air(item)
-                self._edit_file(filename, 'a', encode=True)
-
-
-    def booking(self, filename, url, offset=None, write=False):
-        """Scraper for https://booking.com/"""
-        self.url = url
-        if write:
-            self._edit_file(filename, 'w')
-        else:
-            self._set('boo', offset)
-        response, soup = self._get_response(self.url)
-        
-        for item in soup.select('.sr_property_block'):
-            try:
-                self.get_attr_boo(item) # Gets booking.com attributes
-                self._edit_file(filename, 'a')
-            except UnicodeEncodeError as e:
-                print(e)
-                self.get_attr_boo(item)
-                self._edit_file(filename, 'a', encode=True)
-    
-    def _get_response(self, url):
-        """"Gets headers and response"""
-        
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'}
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'lxml')
-        return response, soup
-
-    def _set(self, site, page):
-        """Sets mode to write or append and sets append url"""
-        if site == 'asp':
-            self.url = f'{self.url}/page/{page}'
-        elif site == 'air':
-            self.url = f'{self.url}&items_offset={page}&section_offset=3'
-        elif site == 'boo':
+    def create_url(self):
+        """Creates url for extra pages"""
+        if self.website == 'aspects':
+            self.url = f'{self.url}/page/{self.page}'
+        elif self.website == 'airbnb':
+            self.url = f'{self.url}&items_offset={self.page}&section_offset=3'
+        elif self.website == 'booking':
             self.url = 'https://www.booking.com/searchresults.en-gb.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQ_YAQHoAQH4AQuIAgGoAgO4Aq7B__4FwAIB0gIkNTIyZjhlMDItNWM3ZC00YzQ5LThlYzAtYmEzN2QyMzk0Zjlj2AIG4AIB&tmpl=searchresults&ac_click_type=b&ac_position=0&checkin_month=12&checkin_monthday=25&checkin_year=2021&checkout_month=1&checkout_monthday=1&checkout_year=2022&class_interval=1&dest_id=-2604050&dest_type=city&from_sf=1&group_adults=4&group_children=0&iata=NQY&label_click=undef&nflt=ht_id%3D201%3Bht_id%3D220%3Bht_id%3D213%3B&no_rooms=2&order=price&percent_htype_apt=1&raw_dest_type=city&room1=A%2CA&room2=A%2CA&sb_price_type=total&search_selected=1&shw_aparth=1&slp_r_match=0&srpvid=0f5708e2731a0009&ss=Newquay%2C%20Cornwall%2C%20United%20Kingdom&ss_raw=newq&ssb=empty&top_ufis=1&rows=25&offset='
-            self.url = f'{self.url}{page}'
-
-    def _edit_file(self, filename, mode, encode=False):
+            self.url = f'{self.url}{self.page}'
+    
+    def edit_file(self, mode, encode=False):
+        """Writes or appends to file"""
         if encode:
-            with open(filename, mode, newline='', encoding="utf-8") as csv_file:
+            with open(self.filename, mode, newline='', encoding="utf-8") as csv_file:
                 csv_writer = csv.writer(csv_file)
                 csv_writer.writerow([self.name, self.price, self.accommodation, self.rooms])
         else:
-            with open(filename, mode, newline='') as csv_file:
+            with open(self.filename, mode, newline='') as csv_file:
                 csv_writer = csv.writer(csv_file)
                 if mode == 'w':
                     csv_writer.writerow(['name', 'price', 'accommodation', 'rooms'])
                 elif mode == 'a':
                     csv_writer.writerow([self.name, self.price, self.accommodation, self.rooms])
+
+    def get_response(self):
+        """"Gets headers and response"""
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'}
+        response = requests.get(self.url, headers=headers)
+        soup = BeautifulSoup(response.content, 'lxml')
+        if self.website == 'aspects':
+            self.block = '.property-box-inner'
+        elif self.website == 'airbnb':
+            self.block = '._8s3ctt'
+        elif self.website == 'booking':
+            self.block = '.sr_property_block'
+        return response, soup
+   
+    def get_attr(self, item):
+        if self.block == '.property-box-inner':
+            self.get_attr_asp(item)
+        elif self.block == '._8s3ctt':
+            self.get_attr_air(item)
+        elif self.block == '.sr_property_block':
+            self.get_attr_boo(item)
 
     def get_attr_asp(self, item):
         """Gets attributes for aspects"""
