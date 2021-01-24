90aa1e2fc8d43ff8e5675300946ae48740a43187
mspencersmith <mspencersmith@outlook.com>
2021-01-24 00:14:38 +0000

    Created Average and min graphs

--- analysis/graphs.py
+++ analysis/graphs.py
@@ -8,10 +8,33 @@ data = os.path.join(BASE_DIR, 'data/average_price.csv')
 
 df = pd.read_csv(data)
 
+months = []
+month_index = []
+get_months = lambda x: [months.append(x), month_index.append(df['month'][df['month']==x].index[0])]
+df['month'].map(get_months, na_action='ignore')
+
+
 print(df)
+plt.style.use('seaborn')
+avg = df.plot.line(x='date', y=['mean', 'median'], title='Average Price Per Week (£)')
 
-df.plot.line(x='date', y='median')
+avg.set_xlabel('Date')
+avg.set_ylabel('Price(£)')
 
+avg.set_xticks(month_ind)
+avg.set_xticklabels(months, fontsize='small')
+
+plt.savefig('graphs/average.png', dpi=400, bbox_inches='tight')
+
+minp = df.plot.line(x='date', y=['min'], title='Average Price Per Week (£)')
+
+minp.set_xlabel('Date')
+minp.set_ylabel('Price(£)')
+
+minp.set_xticks(month_ind)
+minp.set_xticklabels(months, fontsize='small')
+
+plt.savefig('graphs/min.png', dpi=400, bbox_inches='tight')
 plt.show()
 
 
