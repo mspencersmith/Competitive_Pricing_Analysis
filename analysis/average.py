93efebb1091ee948617c5e033af098aa154b155c
mspencersmith <mspencersmith@outlook.com>
2021-01-30 21:16:48 +0000

    Aggregated prices by number of bedroom

--- analysis/average.py
+++ analysis/average.py
@@ -5,23 +5,37 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 def avg_all(file, out_file=None):
-
-    df = get_data(file)
+    """Returns average price of all properties"""
+    df = get_df(file)
     date = df.groupby(['date'])
-
-    average_price = date['price_GBP'].agg(['min','max','mean', 'median'])
-
-    average_price = average_price.round(2)
-
-    print(average_price)
-
+    average_price = average(date['price_GBP'])
     if out_file:
-        output = os.path.join(BASE_DIR, out_file)
-        average_price.to_csv(output)
+        create_csv(out_file, average_price)
+
+def avg_rooms(file, rooms, out_file=None):
+    """Returns average price of properties of given room size"""
+    df = get_df(file)
+    df['rooms'] = df['rooms'].str.lower()
+    filt = df['rooms'].str.contains(f'{rooms} bedrooms', na=False)
+    rooms = df[filt]
+    date = rooms.groupby(['date'])
+    average_price = average(date['price_GBP'])
+    if out_file:
+        create_csv(out_file, average_price)
 
 def get_df(file):
+    """Joins path and opens csv file"""
     data = os.path.join(BASE_DIR, file)
     df = pd.read_csv(data)
     return df
 
-avg_all('data/cleaned_pricing_data.csv','data/average_price.csv')
+def average(date):
+    """Calcutes average and min max of data series"""
+    average_price = date.agg(['min','max','mean', 'median'])
+    average_price = average_price.round(2)
+    print(average_price)
+    return average_price
+
+def create_csv(out_file, average_price):
+    output = os.path.join(BASE_DIR, out_file)
+    average_price.to_csv(output)
