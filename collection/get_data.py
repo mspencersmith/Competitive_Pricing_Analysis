e2db2503e24146400b649f7fd6f7cd9e6a465cd1
mspencersmith <mspencersmith@outlook.com>
2021-02-02 19:18:41 +0000

    Cleaned code in Web_scraper.py and get_data.py

--- collection/get_data.py
+++ collection/get_data.py
@@ -6,13 +6,10 @@ from web_scraper import WebScraper
 from tqdm import tqdm
 from pathlib import Path
 
-
 start = time.time()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-output_file = os.path.join(BASE_DIR, 'data/pricing_date.csv')
-
-WebScraper.edit_file('_', output_file, 'w')
+output_file = os.path.join(BASE_DIR, 'data/test.csv')
 
 with open('dates.json') as f:
     dates = json.load(f)
@@ -22,31 +19,37 @@ with open('urls.json') as f:
 checkins = dates['checkin']
 checkouts = dates['checkout']
 
-for checkin, checkout in zip(checkins, checkouts):
+for checkin, checkout in zip(checkins[10:], checkouts[10:]):
     web1_url = url['web1'].format(checkin=checkin)
     print(f'\n\nAppending website1 data for {checkin}')
-    WebScraper(output_file, web1_url, 'website1', checkin) # First page
+    web1 = WebScraper(web1_url, 'website1', checkin) # First page
+    web1.scrape(output_file)
     for i in tqdm(range(2, 101)):
-        asp = WebScraper(output_file, web1_url, 'website1', checkin, page=i)
-        if not asp.MorePages:
+        web1 = WebScraper(web1_url, 'website1', checkin, page=i)
+        web1.scrape(output_file)
+        if not web1.MorePages:
             break
-    
+
     web2_url = url['web2'].format(checkin=checkin, checkout=checkout)
     print(f'\n\nAppending website2 data for {checkin}')
-    WebScraper(output_file, web2_url, 'website2', checkin, checkout) # First page
+    web2 = WebScraper(web2_url, 'website2', checkin, checkout) # First page
+    web2.scrape(output_file)
     for i in tqdm(range(20, 2001, 20)): # Offset starts at 20 amd increases by 20
-        air = WebScraper(output_file, web2_url, 'website2', checkin, checkout, i)
-        if not air.MorePages:
+        web2 = WebScraper(web2_url, 'website2', checkin, checkout, i)
+        web2.scrape(output_file)
+        if not web2.MorePages:
             break
     
     in_year, in_month, in_day = WebScraper.web3_date('_', checkin)
     out_year, out_month, out_day = WebScraper.web3_date('_', checkout)
     web3_url = url['web3'].format(in_month=in_month, in_day=in_day, in_year=in_year, out_month=out_month, out_day=out_day, out_year=out_year)
     print(f'\n\nAppending website3 data for {checkin}')
-    WebScraper(output_file, web3_url, 'website3', checkin, checkout) # First page
+    web3 = WebScraper(web3_url, 'website3', checkin, checkout) # First page
+    web3.scrape(output_file)
     for i in tqdm(range(25, 2501, 25)): # Offset starts at 25 and increases by 25 
-        boo = WebScraper(output_file, web3_url, 'website3', checkin, checkout, i)
-        if not boo.MorePages:
+        web3 = WebScraper(web3_url, 'website3', checkin, checkout, i)
+        web3.scrape(output_file)
+        if not web3.MorePages:
             break
     
     time.sleep(60)
--- collection/web_scraper.py
+++ collection/web_scraper.py
@@ -2,38 +2,31 @@ import csv
 import json
 import re
 import requests
-
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 class WebScraper:
     """Scrapes holiday websites and returns name, price, type of accommodation, number of rooms"""
 
-    def __init__(self, filename, url, website, checkin, checkout=None, page=None):
+    def __init__(self, url, website, checkin, checkout=None, page=None):
         self.url = url
         self.website = website
-        self.filename = filename
         self.checkin = checkin
         self.checkout = checkout
         self.page = page
         self.MorePages = True
         if page:        
             self.create_url() #Creates url for extra pages
-        self.scrape()
 
-    def scrape(self):
+    def scrape(self, filename):
         """Scrapes website outputs to csv"""
-        soup = self.get_response()
-        self.page_check(soup) # Checks if page number is in web page range
-
-        for item in soup.select(self.block): # Selects block of html where attributes are stored for all websites
-            try:
-                self.get_attr(item)
-                self.edit_file(self.filename, 'a') # Appends to file
-            except UnicodeEncodeError as e:
-                print(e)
-                self.get_attr(item)
-                self.edit_file(self.filename, 'a', encode=True) # Appends to file with encoding
+        soup, block = self.get_response()
+        self.page_check(soup, block) # Checks if page number is in web page range
+        with open(filename, 'a', newline='', encoding="utf-8") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for item in soup.select(block): # Selects block of html where attributes are stored for all websites
+                name, price, accommodation, rooms = self.get_attr(item)
+                csv_writer.writerow([self.checkin, self.website, name, price, accommodation, rooms])
 
     def create_url(self):
         """Creates url for extra pages"""
@@ -62,7 +55,7 @@ class WebScraper:
             day = date[8:10]
         return year, month, day
 
-    def page_check(self, soup):
+    def page_check(self, soup, block):
         """Checks whether page is in web page range"""
         if self.website == 'website2': # Checks if website2 page is in range
             check = '._1h559tl'
@@ -75,22 +68,8 @@ class WebScraper:
                 if count > total:
                     self.MorePages = False
         else:
-            if not soup.select(self.block): # Checks if website1 and website3 page is in range
+            if not soup.select(block): # Checks if website1 and website3 page is in range
                 self.MorePages = False
-
-    def edit_file(self, filename, mode, encode=False):
-        """Writes or appends to file"""
-        if encode:
-            with open(filename, mode, newline='', encoding="utf-8") as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow([self.checkin, self.website, self.name, self.price, self.accommodation, self.rooms])
-        else:
-            with open(filename, mode, newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                if mode == 'w':
-                    csv_writer.writerow(['date', 'website', 'name', 'price_GBP', 'accommodation', 'rooms'])
-                elif mode == 'a':
-                    csv_writer.writerow([self.checkin, self.website, self.name, self.price, self.accommodation, self.rooms])
 
     def get_response(self):
         """"Gets headers and response"""
@@ -98,40 +77,44 @@ class WebScraper:
         response = requests.get(self.url, headers=headers)
         soup = BeautifulSoup(response.content, 'lxml')
         if self.website == 'website1':
-            self.block = '.property-box-inner'
+            block = '.property-box-inner'
         elif self.website == 'website2':
-            self.block = '._8s3ctt'
+            block = '._8s3ctt'
         elif self.website == 'website3':
-            self.block = '.sr_property_block'
-        return soup
+            block = '.sr_property_block'
+        return soup, block
    
     def get_attr(self, item):
         if self.website == 'website1':
-            self.get_attr_1(item)
+            name, price, accommodation, rooms = self.get_attr_1(item)
         elif self.website == 'website2':
-            self.get_attr_2(item)
+            name, price, accommodation, rooms = self.get_attr_2(item)
         elif self.website == 'website3':
-            self.get_attr_3(item)
+            name, price, accommodation, rooms = self.get_attr_3(item)
+        return name, price, accommodation, rooms
 
     def get_attr_1(self, item):
-        """Gets attributes for website1"""
-        self.name = item.select('.property-name')[0].get_text().strip().split('\r')[0]
-        self.price = item.select('.property-price')[0].get_text().strip().split(' ')[0]
-        self.accommodation = None # No Accommodation type given
-        self.rooms = item.select('.property-toptrumps')[0].get_text().strip()
+        """Returns attributes for website1"""
+        name = item.select('.property-name')[0].get_text().strip().split('\r')[0]
+        price = item.select('.property-price')[0].get_text().strip().split(' ')[0]
+        accommodation = None # No Accommodation type given
+        rooms = item.select('.property-toptrumps')[0].get_text().strip()
+        return name, price, accommodation, rooms
 
     def get_attr_2(self, item):
-        """Gets attributes for air bnb"""
-        self.name = item.a['aria-label']
-        self.price = item.select('._ebe4pze')[0].get_text().strip().split(' ')[0]
-        self.accommodation = item.select('._b14dlit')[0].get_text().strip()
-        self.rooms = item.select('._kqh46o')[0].get_text().strip()
+        """Returns attributes for website2"""
+        name = item.a['aria-label']
+        price = item.select('._ebe4pze')[0].get_text().strip().split(' ')[0]
+        accommodation = item.select('._b14dlit')[0].get_text().strip()
+        rooms = item.select('._kqh46o')[0].get_text().strip()
+        return name, price, accommodation, rooms
 
     def get_attr_3(self, item):
-        """Gets attributes for website3.com"""
-        self.name = item.select('.sr-hotel__name')[0].get_text().strip()
-        self.price = item.select('.bui-price-display__value')[0].get_text().strip()
-        self.accommodation = item.select('.room_link')[0].get_text().strip()
-        self.rooms = ''
+        """Returns attributes for website3"""
+        name = item.select('.sr-hotel__name')[0].get_text().strip()
+        price = item.select('.bui-price-display__value')[0].get_text().strip()
+        accommodation = item.select('.room_link')[0].get_text().strip()
+        rooms = ''
         for i in item.select('.c-unit-configuration__item'): # Rooms is given as a list
-            self.rooms = self.rooms + i.get_text().strip() + ' '
+            rooms = rooms + i.get_text().strip() + ' '
+        return name, price, accommodation, rooms
